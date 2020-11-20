7a3119d8408d643c778dc98db3d7213c17d39c4a
shuyin21 <51820731+shuyin21@users.noreply.github.com>
2020-11-20 11:20:02 +0000

    moved our directory and  shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
+import { PersistGate} from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import {store, persistor} from './redux/store';
+
 
 ReactDOM.render(
   <Provider store={store}>
   <BrowserRouter>
+    <PersistGate persistor={persistor}>
     <App />
+    </PersistGate>
   </BrowserRouter>
   </Provider>,
   document.getElementById('root')
