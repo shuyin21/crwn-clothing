71e6d1a76f59862b00605d00a53d651474248fbb
shuyin21 <51820731+shuyin21@users.noreply.github.com>
2020-12-08 06:46:30 +0000

    created user and cart sagas for sign in, sign up and sign in with google

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import './App.css';
-import {createStructuredSelector} from 'reselect';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -10,30 +11,16 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument} from './firebase/firebase.utils';
-import {setCurrentUser} from './redux/user/user.action';
-import {selectCurrentUser} from './redux/user/user.selector';
 
+import { selectCurrentUser } from './redux/user/user.selectors';
+import { checkUserSession } from './redux/user/user.actions';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const {setCurrentUser} = this.props;
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
-
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-              id: snapShot.id,
-              ...snapShot.data()
-          });});}
-
-      setCurrentUser( userAuth);
-      
-    });
+    const { checkUserSession } = this.props;
+    checkUserSession();
   }
 
   componentWillUnmount() {
@@ -48,19 +35,32 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
-          <Route path='/signin' render={() => this.props.currentUser ?
-           (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
+
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-  
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+  checkUserSession: () => dispatch(checkUserSession())
 });
-export default connect(mapStateToProps, mapDispatchToProps) (App);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,3 +1,4 @@
+  
 import CartActionTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
@@ -17,4 +18,8 @@ export const removeItem = item => ({
 export const clearItemFromCart = item => ({
   type: CartActionTypes.CLEAR_ITEM_FROM_CART,
   payload: item
+});
+
+export const clearCart = () => ({
+  type: CartActionTypes.CLEAR_CART
 });
